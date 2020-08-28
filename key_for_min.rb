174bc36a2b7fe5ffc9cf45b6adb0888796fb43db
Jason Markson <jasonmarkson76@gmail.com>
2020-08-28 23:23:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,8 @@ def key_for_min_value(name_hash)
   
   name_hash.each do |key, value|
     if min_key  == nil
-      min_value = value
       min_key = key
+      min_value = value
     elsif value < min_value
       min_value = value 
       min_key = key
